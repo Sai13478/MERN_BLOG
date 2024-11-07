bacd3b2d04f8f1f59ed592a326c076f42c368ae6
Sai Pramod <saipramodvedantam@gmail.com>
2024-11-07 18:46:32 +0530

    1st one

--- server/index.js
+++ server/index.js
@@ -9,9 +9,30 @@ const postRoutes = require("./routes/postRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+
+// CORS setup
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      // Get the allowed origins from environment variables
+      const allowedOrigins = [
+        process.env.CLIENT_URL, // Production URL from .env file
+        "http://localhost:3000", // Localhost URL for development
+      ];
+
+      // If the origin is in the allowed list, allow it, otherwise block it
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
@@ -21,6 +42,7 @@ app.use("/api/posts", postRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+// Connect to MongoDB and start the server
 connect(process.env.MONGO_URI)
   .then(
     app.listen(process.env.PORT || 5000, () =>
